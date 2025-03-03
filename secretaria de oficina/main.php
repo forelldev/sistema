8087c842dd8f47e38f5b09441631c5eaa8da9d9e
Dave1911 <davidfelipe1911@gmail.com>
2025-03-03 11:24:42 -0400

    Correcion en el main php y nuevo botones

--- secretaria de oficina/main.php
+++ secretaria de oficina/main.php
@@ -13,9 +13,15 @@ include("control/validar_rol.php");
 </head>
 <body class="container-body">
     <header class="header-main">
+        <div class="cajadetexto-main">
     <a href="system_help.php" class="link-main">Sistema de solicitud de ayudas</a>
+        </div>
+        <div class="cajadetexto-main">
     <a href="configuracion_user.php" class="link-main">Configuración de usuario</a>
+        </div>
+        <div class="cajadetexto-main">
     <a href=".././control general/logout.php" class="link-main">Cerrar Sesión</a>
+        </div>
     </header>
 </body>
 </html>
