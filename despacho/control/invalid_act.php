7e2866e8ee7fe5fe6baa8a22091b7b9183acd894
Forell - PC <carlossoteldo11@gmail.com>
2025-03-15 16:41:19 -0400

    fix, Corregido bug al dar invalido el documento

--- despacho/control/invalid_act.php
+++ despacho/control/invalid_act.php
@@ -1,7 +1,7 @@
 <?php 
 include("../.././control general/conexion.php"); 
 $id = $_GET['id'];
-$estado = "En espera del documento físico para ser procesado 1/3";
+$estado = "En espera del documento físico para ser procesado 0/3";
 $consulta = $conexion->query("UPDATE system_help SET estado = '$estado', descripcion = '' WHERE id = '$id'");
 header("Location: ../system_help.php");
 exit();
